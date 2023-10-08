da81037ebf9f87b88d38dcd93ac160c41b15734a
Mohamed Ahmed <103213877+m7meda7medd@users.noreply.github.com>
2023-10-09 01:11:20 +0300

    keyboard_monitor code and DHT11 publish script updated

--- Pi_publish.py
+++ Pi_publish.py
@@ -1,8 +1,10 @@
 import paho.mqtt.client as mqtt
 import time
-
+import ssl
+import Adafruit_DHT
+import random
 # Define the MQTT broker's address and port
-broker_address = "" #broker address 
+broker_address = "9f546752e5564dcaae715934de954a6f.s2.eu.hivemq.cloud" #broker address 
 broker_port = 8883
 
 # Create an MQTT client instance
@@ -10,14 +12,28 @@ client = mqtt.Client("RaspberryPiPublisher")
 
 # Connect to the broker
 client.connect(broker_address, broker_port)
-
+client.username_pw_set("omar2033","Panzer123") # User Credentials for Broker 
+client.tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_NONE, tls_version=ssl.PROTOCOL_TLSv1_2) #Tls Authentication 
+client.connect(broker_address, broker_port)    
+print("Connected on Broker !")
+sensor=Adafruit_DHT.DHT11 
+gpio=17
+print("DHT_initalized")
 while True:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+
     # Publish a message to a topic
-    topic = "your/topic"  
-    message = "Hello, MQTT!"  
-    client.publish(topic, message)
-    print(f"Published: {message} to topic: {topic}")
-    time.sleep(5)  
+    temp_topic = "temperature"
+    humd_topic = "humidity"    
+    client.publish(temp_topic, temperature)
+    client.publish(humd_topic, humidity)
+    print(f"Published: {temperature} to topic: {temp_topic}")
+    print(f"Published: {humidity} to topic: {humd_topic}")
+    time.sleep(2)  
 
 #if you want to disconnect 
 client.disconnect()
--- keyboard_monitor.py
+++ keyboard_monitor.py
@@ -1,8 +1,11 @@
 from pynput.keyboard import Key, Listener
-
+import paho.mqtt.client as mqtt
+import ssl
 # Define a callback function to handle key events
 message = b'S'
+lastmessage =b'S'
 def on_key_press(key):
+    global message , lastmessage 
     try:
         # You can add your custom logic here to process the key press event
         if (key == Key.up) :
@@ -15,28 +18,52 @@ def on_key_press(key):
             message = b"L" 
         elif (key.char == 'q') :
             message =b"q" 
-        elif (key.char == 'm') :
+        elif (key.char == 'm') :    
             message =b"5"
+        elif (key.char == 'o') :
+            message = b"O"
+        elif (key.char == 'c') :
+            message = b"C"
         elif (key.char == 'l') :
             message =b"1"
-        if message in [b'B',b'L',b'L',]           
+        else :
+            pass      
+        if (message in [b'B',b'L',b'R',b'F',b'O',b'C'] ) and (message != lastmessage):
+            topic = "directions"   
+            client.publish(topic,payload =message)
+            print(f"Published: {message} to topic: {topic}")
+            lastmessage = message 
+        else :
+            if message != lastmessage :
+                topic = "speed_car"   
+                client.publish(topic, message)
+                print(f"Published: {message} to topic: {topic}")
+                lastmessage = message 
+
+
     except Exception as e:
         print(f"Error: {str(e)}")
 
 def on_key_release(key):
-    #if key != key and key != key.m  and key != key.l :
-    message = b"S"
-
+    global message , lastmessage 
+    if message in [b'B',b'L',b'R',b'F'] :
+            topic = "directions"   
+            message = b"S"
+            lastmessage = b"S"
+            client.publish(topic, message)
+            print(f"Published: {message} to topic: {topic}")
 # Create a listener that monitors keyboard events
 # Define the MQTT broker's address and port
 broker_address = "9f546752e5564dcaae715934de954a6f.s2.eu.hivemq.cloud" #broker address 
 broker_port = 8883
 
 # Create an MQTT client instance
-client = mqtt.Client("RaspberryPiPublisher")
+client = mqtt.Client("KeyboardPublisher")
 
 # Connect to the broker
-client.connect(broker_address, broker_port)
-
+client.username_pw_set("omar2033","Panzer123") # User Credentials for Broker 
+client.tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_NONE, tls_version=ssl.PROTOCOL_TLSv1_2) #Tls Authentication 
+client.connect(broker_address, broker_port)    
+print("Connected on Broker !")
 with Listener(on_press=on_key_press, on_release=on_key_release) as listener:
-    listener.join()
+    listener.join(client.loop_forever())
