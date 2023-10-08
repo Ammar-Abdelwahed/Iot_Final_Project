fc9e57cb834997bdf9a036f28f58552dbe35b714
Ammar-Abdelwahed <ammarabdelwahed12211@gmail.com>
2023-10-09 00:16:01 +0300

    edit

--- keyboard_monitor.py
+++ keyboard_monitor.py
@@ -1,30 +1,42 @@
 from pynput.keyboard import Key, Listener
 
 # Define a callback function to handle key events
+message = b'S'
 def on_key_press(key):
     try:
         # You can add your custom logic here to process the key press event
         if (key == Key.up) :
-            print(b"F")
+            message =b"F"
         elif (key == Key.down) :
-            print(b"B")
+            message=b"B"
         elif  (key == Key.right) :
-            print(b"R")
+            message =b"R"
         elif (key == Key.left) :
-            print(b"L") 
+            message = b"L" 
         elif (key.char == 'q') :
-            print(b"q") 
+            message =b"q" 
         elif (key.char == 'm') :
-            print(b"5")
+            message =b"5"
         elif (key.char == 'l') :
-            print(b"1")       
+            message =b"1"
+        if message in [b'B',b'L',b'L',]           
     except Exception as e:
         print(f"Error: {str(e)}")
 
 def on_key_release(key):
     #if key != key and key != key.m  and key != key.l :
-    print("S")
+    message = b"S"
 
 # Create a listener that monitors keyboard events
+# Define the MQTT broker's address and port
+broker_address = "9f546752e5564dcaae715934de954a6f.s2.eu.hivemq.cloud" #broker address 
+broker_port = 8883
+
+# Create an MQTT client instance
+client = mqtt.Client("RaspberryPiPublisher")
+
+# Connect to the broker
+client.connect(broker_address, broker_port)
+
 with Listener(on_press=on_key_press, on_release=on_key_release) as listener:
     listener.join()
